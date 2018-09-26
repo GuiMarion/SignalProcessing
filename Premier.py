edd0193fffb93599b2204a8322ccfdee3df188fb
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-09-26 18:49:27 +0200

    add methods and fix noise

--- Premier.py
+++ Premier.py
@@ -2,91 +2,231 @@ import random
 import matplotlib.pyplot as plt
 import time
 
+class functionX:
 
-def x(i, N):
+    def __init__(self, N):
+        self.N = N
+        self.X = []
+        for i in range(N):
+            self.X.append(i+1)
 
-	X = []
-
-	for e in range(N):
-		X.append(e+1)
-
-	if i < N:
-		return X[i]
-
-	else :
-		return 0
-
-def h(i, N):
-
-	X = []
-
-	for i in range(N):
-		X.append(1)
-
-	if i < N:
-		return X[i]
-
-	else :
-		return 0
+    def __str__(self):
+        return str(self.X)
 
 
-def h2(i, M):
+    def plot(self):
+        plt.plot(self.X)
+        plt.show()
 
-	X = []
 
-	for i in range(M):
-		X.append(1/M)
+    def getElem(self,i):
+        if i < self.N and i >= 0:
+            return self.X[i]
 
-	if i < M:
-		return X[i]
+        else :
+            return 0
 
-	else :
-		return 0
+    def getElems(self, imin, imax):
 
-def xBruit(i, N):
+        if imax < self.N:
+            return self.X[imin:imax]
+        else:
+            return self.X[imin : self.N] + [0]*(imax-self.N)
+        
 
-	X = []
+    def getAllValues(self):
+        return self.X
 
-	for e in range(N):
-		X.append(e+1 + random.random()*10)
+    def getSize(self):
+        return self.N
 
-	if i < N and i >=0:
-		return X[i]
+    def printSignal(self, imin, imax):
+        print(self.getElems(imin,imax))
 
-	else :
-		return 0
 
-def conv(N, M):
 
-	Max = max(N,M)
+class functionH:
 
-	Y = []
+    def __init__(self, N):
+        self.N = N
+        self.X = []
+        for i in range(N):
+            self.X.append(1)
 
-	for n in range(Max):
+    def __str__(self):
+        return str(self.X)
 
-		ret = 0
-		for i in range(2*Max):
-			ret += h2(i, N)*xBruit(n-i, M)
 
-		Y.append(ret)
+    def plot(self):
+        plt.plot(self.X)
+        plt.show()
 
-	return Y
 
-N = 200
+    def getElem(self,i):
+        if i < self.N and i >= 0:
+            return self.X[i]
+        else :
+            return 0
 
-X = []
+    def getAllValues(self):
+        return self.X
 
-for i in range(N):
-	X.append(xBruit(i, N))
+    def getElems(self, imin, imax):
 
-time_start = time.clock()
-Y = conv(5,N)
-time_elapsed = (time.clock() - time_start)
+        if imax < self.N:
+            return self.X[imin:imax]
+        else:
+            return self.X[imin : self.N] + [0]*(imax-self.N)
+        
+    def getSize(self):
+        return self.N
 
-print(time_elapsed)
+    def printSignal(self, imin, imax):
+        print(self.getElems(imin,imax))
 
-plt.plot(Y, 'k')
-plt.plot(X)
-plt.show()
+
+class functionHN:
+
+    def __init__(self, N):
+        self.N = N
+        self.X = []
+        for i in range(N):
+            self.X.append(1/N)
+
+    def __str__(self):
+        return str(self.X)
+
+
+    def plot(self):
+        plt.plot(self.X)
+        plt.show()
+
+
+    def getElem(self,i):
+        if i < self.N and i >= 0:
+            return self.X[i]
+
+        else :
+            return 0
+
+    def getElems(self, imin, imax):
+
+        if imax < self.N:
+            return self.X[imin:imax]
+        else:
+            return self.X[imin : self.N] + [0]*(imax-self.N)
+        
+
+    def getAllValues(self):
+        return self.X
+
+    def getSize(self):
+        return self.N
+
+    def printSignal(self, imin, imax):
+        print(self.getElems(imin,imax))
+
+
+class functionXNoisy:
+
+    # B niveau de bruit
+    def __init__(self, N, B):
+        self.N = N
+        self.X = []
+        for i in range(N):
+            self.X.append(i+1 - random.random()*B +0.5)
+
+    def __str__(self):
+        return str(self.X)
+
+
+    def plot(self):
+        plt.plot(self.X)
+        plt.show()
+
+    def getElem(self,i):
+        if i < self.N and i >= 0:
+            return self.X[i]
+
+        else :
+            return 0
+
+    def getElems(self, imin, imax):
+
+        if imax < self.N:
+            return self.X[imin:imax]
+        else:
+            return self.X[imin : self.N] + [0]*(imax-self.N)
+        
+    def getAllValues(self):
+        return self.X
+
+    def getSize(self):
+        return self.N
+
+    def printSignal(self, imin, imax):
+        print(self.getElems(imin,imax))
+
+
+def conv(x, h):
+
+    time_start = time.clock()
+    Max = max(x.getSize(), h.getSize())
+
+    Y = []
+
+    for n in range(Max):
+
+        ret = 0
+        for i in range(2*Max):
+            ret += h.getElem(i)*x.getElem(n-i)
+
+        Y.append(ret)
+
+    time_elapsed = (time.clock() - time_start)
+    print()
+    print("Computation time:",time_elapsed)
+    print()
+
+    return Y
+
+def main():
+
+    # Simple
+    x = functionX(10)
+    h = functionH(30)
+
+    Y = conv(x,h)
+
+    plt.plot(Y, 'k')
+    plt.plot([x.getElem(i) for i in range(30)])
+    plt.show()
+
+    # Noisy 
+    x = functionXNoisy(10, 1)
+    h = functionH(30)
+
+    Y = conv(x,h)
+
+    plt.plot(Y, 'k')
+    plt.plot([x.getElem(i) for i in range(30)])
+    plt.show()
+
+    # Noisy + h normalized
+    x = functionXNoisy(10, 1)
+    h = functionHN(30)
+
+    Y = conv(x,h)
+
+    plt.plot(Y, 'k')
+    plt.plot([x.getElem(i) for i in range(30)])
+    plt.show()
+
+
+
+main()
+
+
+
 
 
